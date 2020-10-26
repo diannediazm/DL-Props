e06f6054b0253298dd9b53577690ed26ac563fde
Dianne <dianne.diazm@gmail.com>
2020-10-25 21:26:28 -0300

    primer commit

---
+++ src/components/Tareas.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <form @submit.prevent="enviarTareas">
+      <div class="form-group">
+        <label for="tarea">Ingresa una tarea</label>
+        <input type="text" class="form-control" v-model="tarea">
+        <small id="emailHelp" class="form-text text-muted">Escribe aquí</small>
+      </div>
+      <div class="form-group">
+        <label for="imagen">Ingresa una imagen</label>
+        <input type="text" class="form-control" v-model="imagen">
+        <small id="emailHelp" class="form-text text-muted">Pega un link</small>
+      </div>
+      <div class="form-group">
+        <label for="descripcion">Ingresa una descripción de tu tarea</label>
+        <input type="text" class="form-control" v-model="descripcion">
+        <small id="emailHelp" class="form-text text-muted">Escribe aquí</small>
+      </div>
+      <button type="submit" class="btn btn-primary bg-info">Agregar tarea</button>
+  </form>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Tareas',
+    data() {
+      return {
+        tarea: '',
+        imagen: '',
+        descripcion: ''
+      }
+    },
+    methods: {
+      enviarTareas(){
+        if (this.tarea && this.imagen && this.descripcion) {
+          let listaTareas = {
+            tarea: this.tarea,
+            imagen: this.imagen,
+            descripcion: this.descripcion
+        };
+        this.$emit('enviandoTareas', listaTareas);
+        this.titulo = "";
+        this.imagen = "";
+        this.descripcion = "";
+        }
+      }  
+    },
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+form {
+  width: 50%;
+  margin-left: 50px;
+  margin-top: 50px;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-Vue.config.productionTip = false
+library.add(faGithub);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
